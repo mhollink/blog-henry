2bacd6eca5f6fbc13116514ad38d39c053a5759f
m.hollink <m.hollink@dictu.nl>
2025-11-09 19:57:01 +0100

    Fix the suggestions display

--- src/components/read-next/ReadNext.tsx
+++ src/components/read-next/ReadNext.tsx
@@ -4,10 +4,15 @@ import {PostCard} from "../post-card/PostCard.tsx";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import {useTheme} from "@mui/material/styles"
 
 export const ReadNext = ({currentBlog}: { currentBlog: PostMeta }) => {
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
     const getRecommendations = useRecommendations();
-    const recommendations = getRecommendations(currentBlog)
+    const recommendations = getRecommendations(currentBlog, isMobile ? 2 : 3)
 
     return (
         <Stack gap={2}>
@@ -16,7 +21,7 @@ export const ReadNext = ({currentBlog}: { currentBlog: PostMeta }) => {
             </Typography>
             <Grid container spacing={2} columns={12}>
                 {recommendations.map(blog =>
-                    <Grid size={4} key={blog.filename}>
+                    <Grid size={{ md: 4, sm: 6, xs: 12}} key={blog.filename}>
                         <PostCard post={blog} handleFocus={() => null} handleBlur={() => null} focussed={false}/>
                     </Grid>)}
 
