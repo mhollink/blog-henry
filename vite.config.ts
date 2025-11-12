7161a936699ac79830d802dfc1615b6ec960a155
m.hollink <m.hollink@dictu.nl>
2025-11-12 09:15:16 +0100

    Split the build in to smaller chunks

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,29 @@
 import {defineConfig} from 'vite'
+import { analyzer } from 'vite-bundle-analyzer'
 import react from '@vitejs/plugin-react'
 import blogReloadPlugin from "./scripts/vite-reload-posts-plugin.ts"
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), blogReloadPlugin()],
+    plugins: [
+        react(),
+        blogReloadPlugin(),
+        analyzer({
+            analyzerMode: 'static',
+            fileName: 'analyse.html',
+        })
+    ],
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        if (id.includes('react')) return 'react';
+                        if (id.includes('@mui') || id.includes('@emotion')) return 'react-mui';
+                        return 'vendor';
+                    }
+                },
+            },
+        },
+    },
 })
