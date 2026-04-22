b0ac9c70bb653d195705bfd770bd26b571268b2e
m.hollink <m.hollink@dictu.nl>
2026-04-22 10:56:55 +0200

    Voeg een astro-svelte app toe met een mooi blog-theme

---
+++ app/site.config.ts
@@ -0,0 +1,38 @@
+import siteConfig from "./src/lib/config";
+
+const config = siteConfig({
+	title: "Henry Hollink",
+	prologue: "Never say goodbye,\nbecause goodbye means going away\nand going away means forgetting.",
+	author: {
+		name: "Esmee & Marcel Hollink - van Kuijk",
+		email: "contact@marcelhollink.nl",
+		link: "https://marcel.hollink.dev"
+	},
+	description: "A modern Astro theme focused on content creation.",
+	copyright: {
+		type: "CC BY-NC-ND 4.0",
+		year: "2026"
+	},
+	timezone: "UTC",
+	i18n: {
+		locales: ["nl"],
+		defaultLocale: "nl"
+	},
+	pagination: {
+		note: 15,
+		jotting: 24
+	},
+	heatmap: {
+		unit: "day",
+		weeks: 20
+	},
+	feed: {
+		section: "*",
+		limit: 20
+	},
+	latest: "*"
+});
+
+export const monolocale = Number(config.i18n.locales.length) === 1;
+
+export default config;
